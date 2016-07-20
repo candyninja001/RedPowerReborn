e93e907635a7b4a125563479e77cef0e85dcce03
CandyNinja <candyninja001@gmail.com>
2016-07-20 13:25:54 -0700

    Fixed some issues with flax

--- src/main/java/candy/redpowerreborn/block/BlockFlax.java
+++ src/main/java/candy/redpowerreborn/block/BlockFlax.java
@@ -8,10 +8,12 @@ import candy.redpowerreborn.item.RedPowerItems;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockCrops;
 import net.minecraft.block.BlockDoublePlant;
+import net.minecraft.block.SoundType;
 import net.minecraft.block.properties.IProperty;
 import net.minecraft.block.properties.PropertyEnum;
 import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
@@ -33,9 +35,18 @@ public class BlockFlax extends BlockCrops {
 			new AxisAlignedBB(0.0D, 0.0D, 0.0D, 1.0D, 0.84375D, 1.0D),
 			new AxisAlignedBB(0.0D, 0.0D, 0.0D, 1.0D, 0.84375D, 1.0D),
 			new AxisAlignedBB(0.0D, 0.0D, 0.0D, 1.0D, 1.0D, 1.0D) };
-	public static final PropertyEnum<BlockDoublePlant.EnumBlockHalf> HALF = PropertyEnum
-			.<BlockDoublePlant.EnumBlockHalf> create("half",
-					BlockDoublePlant.EnumBlockHalf.class);
+	public static final PropertyEnum<BlockDoublePlant.EnumBlockHalf> HALF = PropertyEnum.<BlockDoublePlant
+			.EnumBlockHalf> create("half", BlockDoublePlant.EnumBlockHalf.class);
+	
+	protected BlockFlax()
+    {
+        this.setDefaultState(this.blockState.getBaseState().withProperty(AGE, 0).withProperty(HALF, BlockDoublePlant.EnumBlockHalf.LOWER));
+        this.setTickRandomly(true);
+        this.setCreativeTab((CreativeTabs)null);
+        this.setHardness(0.0F);
+        this.setSoundType(SoundType.PLANT);
+        this.disableStats();
+    }
 
 	protected Item getSeed() {
 		return RedPowerItems.FLAX_SEED;
@@ -45,26 +56,21 @@ public class BlockFlax extends BlockCrops {
 		return Items.STRING;
 	}
 
-	public AxisAlignedBB getBoundingBox(IBlockState state, IBlockAccess source,
-			BlockPos pos) {
-		return FLAX_AABB[((Integer) state.getValue(this.getAgeProperty()))
-				.intValue()];
+	public AxisAlignedBB getBoundingBox(IBlockState state, IBlockAccess source, BlockPos pos) {
+		return FLAX_AABB[((Integer) state.getValue(this.getAgeProperty())).intValue()];
 	}
 
 	protected BlockStateContainer createBlockState() {
 		return new BlockStateContainer(this, new IProperty[] { AGE, HALF });
 	}
 
-	protected void checkAndDropBlock(World worldIn, BlockPos pos,
-			IBlockState state) {
+	protected void checkAndDropBlock(World worldIn, BlockPos pos, IBlockState state) {
 		if (!this.canBlockStay(worldIn, pos, state)) {
 			boolean flag = state.getValue(HALF) == BlockDoublePlant.EnumBlockHalf.UPPER;
 			BlockPos blockpos = flag ? pos : pos.up();
 			BlockPos blockpos1 = flag ? pos.down() : pos;
-			Block block = (Block) (flag ? this : worldIn
-					.getBlockState(blockpos).getBlock());
-			Block block1 = (Block) (flag ? worldIn.getBlockState(blockpos1)
-					.getBlock() : this);
+			Block block = (Block) (flag ? this : worldIn.getBlockState(blockpos).getBlock());
+			Block block1 = (Block) (flag ? worldIn.getBlockState(blockpos1).getBlock() : this);
 
 			if (!flag)
 				this.dropBlockAsItem(worldIn, pos, state, 0); // Forge move
@@ -77,8 +83,7 @@ public class BlockFlax extends BlockCrops {
 			}
 
 			if (block1 == this) {
-				worldIn.setBlockState(blockpos1, Blocks.AIR.getDefaultState(),
-						3);
+				worldIn.setBlockState(blockpos1, Blocks.AIR.getDefaultState(), 3);
 			}
 		}
 	}
@@ -87,21 +92,16 @@ public class BlockFlax extends BlockCrops {
 		if (state.getValue(HALF) == BlockDoublePlant.EnumBlockHalf.UPPER) {
 			return worldIn.getBlockState(pos.down()).getBlock() == this;
 		} else {
-			IBlockState iblockstate = worldIn.getBlockState(pos.up());
-			return iblockstate.getBlock() == this
-					&& super.canBlockStay(worldIn, pos, iblockstate);
+			return super.canBlockStay(worldIn, pos, state);//TODO this is broken
 		}
 	}
 
 	public IBlockState withAge(int age) {
 		if (age > 7)
-			return this
-					.getDefaultState()
-					.withProperty(AGE, 7)
-					.withProperty(this.HALF,
-							BlockDoublePlant.EnumBlockHalf.UPPER);
-		return this.getDefaultState().withProperty(this.getAgeProperty(),
-				Integer.valueOf(age)).withProperty(HALF, BlockDoublePlant.EnumBlockHalf.LOWER);
+			return this.getDefaultState().withProperty(AGE, 7).withProperty(this.HALF,
+					BlockDoublePlant.EnumBlockHalf.UPPER);
+		return this.getDefaultState().withProperty(this.getAgeProperty(), Integer.valueOf(age)).withProperty(HALF,
+				BlockDoublePlant.EnumBlockHalf.LOWER);
 	}
 
 	/**
@@ -111,6 +111,16 @@ public class BlockFlax extends BlockCrops {
 	public IBlockState getStateFromMeta(int meta) {
 		return this.withAge(meta);
 	}
+	
+	/**
+     * Convert the BlockState into the correct metadata value
+     */
+    public int getMetaFromState(IBlockState state)
+    {
+    	if(state.getValue(HALF) == BlockDoublePlant.EnumBlockHalf.UPPER)
+    		return 8;
+        return this.getAge(state);
+    }
 
 	@Override
 	public void grow(World worldIn, BlockPos pos, IBlockState state) {
@@ -140,8 +150,7 @@ public class BlockFlax extends BlockCrops {
 	}
 
 	@Override
-	public void updateTick(World worldIn, BlockPos pos, IBlockState state,
-			Random rand) {
+	public void updateTick(World worldIn, BlockPos pos, IBlockState state, Random rand) {
 		this.checkAndDropBlock(worldIn, pos, state);
 
 		if (state.getValue(HALF) == BlockDoublePlant.EnumBlockHalf.LOWER) {
@@ -154,10 +163,8 @@ public class BlockFlax extends BlockCrops {
 					if (rand.nextInt((int) (25.0F / f) + 1) == 0) {
 						if (i == 6) {
 							if (worldIn.isAirBlock(pos.up())) {
-								worldIn.setBlockState(pos.up(),
-										this.withAge(i + 2), 2);
-								worldIn.setBlockState(pos, this.withAge(i + 1),
-										2);
+								worldIn.setBlockState(pos.up(), this.withAge(i + 2), 2);
+								worldIn.setBlockState(pos, this.withAge(i + 1), 2);
 							}
 						} else {
 							worldIn.setBlockState(pos, this.withAge(i + 1), 2);
@@ -180,8 +187,7 @@ public class BlockFlax extends BlockCrops {
 		}
 	}
 
-	public void onBlockHarvested(World worldIn, BlockPos pos,
-			IBlockState state, EntityPlayer player) {
+	public void onBlockHarvested(World worldIn, BlockPos pos, IBlockState state, EntityPlayer player) {
 		if (state.getValue(HALF) == BlockDoublePlant.EnumBlockHalf.UPPER) {
 			if (worldIn.getBlockState(pos.down()).getBlock() == this) {
 				if (!player.capabilities.isCreativeMode) {
@@ -204,8 +210,7 @@ public class BlockFlax extends BlockCrops {
 	/**
 	 * Whether this IGrowable can grow
 	 */
-	public boolean canGrow(World worldIn, BlockPos pos, IBlockState state,
-			boolean isClient) {
+	public boolean canGrow(World worldIn, BlockPos pos, IBlockState state, boolean isClient) {
 		if (state.getValue(AGE) == 6) {
 			return worldIn.isAirBlock(pos.up());
 		}
@@ -215,8 +220,7 @@ public class BlockFlax extends BlockCrops {
 		return true;
 	}
 
-	public boolean canUseBonemeal(World worldIn, Random rand, BlockPos pos,
-			IBlockState state) {
+	public boolean canUseBonemeal(World worldIn, Random rand, BlockPos pos, IBlockState state) {
 		if (state.getValue(AGE) == 6) {
 			return worldIn.isAirBlock(pos.up());
 		}
